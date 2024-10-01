4777c35a5b6de714b6a6cf0939aea3cc8df980e2
sanjeevkr07 <sanjeevcoolkvs@gmail.com>
2024-10-02 02:58:08 +0530

    initital project setup

---
+++ src/main/java/com/jlfitness/jlfitness_server/JlfitnessServerApplication.java
@@ -0,0 +1,13 @@
+package com.jlfitness.jlfitness_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlfitnessServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlfitnessServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlfitness/jlfitness_server/JlfitnessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlfitness.jlfitness_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlfitnessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
